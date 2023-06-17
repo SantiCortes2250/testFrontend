a8f4f6306c1811f42bc2bd09515ea83cc38a1ba8
SantiCortes2250 <santicortesrincon15@gmail.com>
2023-06-17 00:32:43 -0500

    implementando swiper

--- src/components/Layouts/MainDashboard.jsx
+++ src/components/Layouts/MainDashboard.jsx
@@ -3,29 +3,37 @@ import barChartbg from "../../assets/images/barChartbg.png";
 import bestSellingbg from "../../assets/images/bestSellingbg.png";
 import chartSelling from "../../assets/images/chartSelling.png";
 import Charts from "../UI/Charts";
-import Galleries from "../UI/galleries";
 import style from "../../styles/dashboard.module.css";
+import Galleries from "../UI/Galleries";
 
 const MainDashboard = () => {
   return (
     <div className={style.main_dashboard}>
-      <div className={style.container_chartbest}>
-        <img
-          src={chartSelling}
-          alt="chartSelling"
-          className={style.chartSelling}
-        />
-        <img
-          src={bestSellingbg}
-          alt="bestSellingbg"
-          className={style.bestSellingbg}
-        />
+      <div className={style.first_chart}>
+        <div className={style.container_chartbest}>
+          <img
+            src={chartSelling}
+            alt="chartSelling"
+            className={style.chartSelling}
+          />
+          <img
+            src={bestSellingbg}
+            alt="bestSellingbg"
+            className={style.bestSellingbg}
+          />
+        </div>
+        <Charts />
       </div>
+      <div className={style.second_chart}>
       <div className={style.container_barChart}>
         <img src={barChartbg} alt="barChartbg" className={style.barChartbg} />
       </div>
-      <Charts />
+      
       <Galleries />
+
+      </div>
+      
+     
     </div>
   );
 };
--- src/components/UI/Galleries.jsx
+++ src/components/UI/Galleries.jsx
@@ -1,15 +1,43 @@
-import React from 'react'
-import style from '../../styles/gallerie.module.css' 
+import React, { useState, useEffect } from "react";
+import style from "../../styles/gallerie.module.css";
+import img from "../../assets/images/barChartbg.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
+import axios from "axios";
 
 const Galleries = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const url = "https://picsum.photos/v2/list?page=2&limit=100";
+
+    const getImages = async () => {
+      const res = await axios.get(url);
+      setImages(res.data);
+    };
+
+    getImages();
+  }, []);
+
   return (
     <div className={style.gallerie}>
-    <h2 className={style.title}>Event Galleries</h2>
-    <div className={style.images_gallerie}>
+      <h2 className={style.title}>Event Galleries</h2>
 
+      <Swiper
+        navigation={true}
+        modules={[Navigation]}
+        className={style.images_gallerie}
+      >
+        {images.map((image) => (
+          <SwiperSlide className={style.swiper} key={image.id}>
+            <img src={image.download_url} alt={image.author} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Galleries
+export default Galleries;
